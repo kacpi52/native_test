a0025efb2a8d4dac03c609da5fea9533b077a2d0
kacpi52 <kacperkrajewski52@gmail.com>
2021-12-30 19:00:51 +0100

    start

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import Location from 'expo-location';
 
 export default function App() {
+  const {errorMessage, setErrorMessage} = useState(null);
+
+  useEffect(()=>{
+    load()  // funkcja async wywolana pozniej 
+  },[])
+  const load = async () =>{
+    try {
+      let { status } = await Location.requestBackgroundPermissionsAsync()
+      if (status !='granted'){
+        setErrorMessage('Access to location is needed to run the app.')
+        return 
+      }
+      const telLocation = await Location.getCurrentPositionAsync();
+      const {latitude , longitude} = telLocation.coords;
+      alert(`Latitude : ${latitude} , Longitude : ${longitude}`);
+
+    } catch (error){ 
+      
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! szczurze</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#055255',
     alignItems: 'center',
     justifyContent: 'center',
   },
